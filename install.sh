cc0467a4bbefa6718eb511f47f9df6a9cbf14979
Brendan Hollaway <bhollaway@bostondynamics.com>
2023-02-06 16:48:20 -0500

    Add install for powerline fonts to install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,6 @@ cp ~/dotfiles/tmux/.tmux.conf.no_autoload ~/.
 # TODO: make a version of my .bashrc that I can just `>>` on to the end
 # of the existing one, and do some tests to see if the .bashrc exists, etc.
 cp ~/dotfiles/.bashrc ~/.
+
+# Install powerline fonts
+mkdir -p ~/.local/share/fonts cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
